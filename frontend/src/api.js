007a49f2b7bb1e57d1d5adba2923c7c1a1496c02
ronnieboniface <86811555+ronnieboniface@users.noreply.github.com>
2021-07-29 17:37:32 -0400

    added patch function to api file

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -36,6 +36,36 @@ const postBook = () => {
 };
 
 // Update a pre-existing book and update it on the DOM.
+const editBook = (id, bookDiv) => {
+  const editForm = document.querySelector('.edit-form');
+  const title = document.getElementById('update-title').value;
+  const author = document.getElementById('update-author').value;
+  const genre = document.getElementById('update-genre').value;
+  const publisher = document.getElementById('update-publisher').value;
+
+  const bookObj = {
+    title: title,
+    author: author,
+    genre: genre,
+    publisher: publisher
+  };
+
+  const configObj = {
+    method: 'PATCH',
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(bookObj)
+  };
+
+  fetch(`${baseUrl}/${id}`, configObj)
+  .then(resp => resp.json())
+  .then((data) => {
+    render(bookDiv, data)
+  });
+  editForm.remove();
+};
 
 
 // Delete a book and remove it from the DOM.
